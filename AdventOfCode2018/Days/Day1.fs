7f7c26028edd56a3e94f590286bf143b3eec6c76
Kevin Boets <kevboets@gmail.com>
2018-12-01 08:30:26 +0100

    Day1 done!

--- AdventOfCode2018/Days/Day1.fs
+++ AdventOfCode2018/Days/Day1.fs
@@ -1,9 +1,29 @@
 ﻿module Day1
 
+open Day1Input
 
-let day1Silver = "TODO"
+let day1Silver = 
+    input
+    |> fun s -> s.Split [|'\n'|]
+    |> Seq.toList
+    |> List.map int
+    |> List.sum
+    |> string
 
 
+let rec rotate acc seen currentRotation fullRotation =
+    let alreadySeen = List.contains acc seen
+    match alreadySeen with
+    | true -> acc
+    | false -> 
+        match currentRotation with
+        | head :: tail -> rotate (acc + head) (acc :: seen) tail fullRotation
+        | [] -> rotate acc seen fullRotation fullRotation
 
-
-let day1Gold = "TODO"
+let day1Gold = 
+    input
+    |> fun s -> s.Split [|'\n'|]
+    |> Seq.toList
+    |> List.map int64
+    |> fun changes -> rotate (int64 0) [] changes changes
+    |> string
